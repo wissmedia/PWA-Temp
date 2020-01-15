55fd408c33da808b732c2922029b8683712e7c36
wissmedia <44659148+wissmedia@users.noreply.github.com>
2020-01-15 21:47:07 +0800

    .

--- js/db.js
+++ js/db.js
@@ -5,6 +5,7 @@ db.collection('recipes').onSnapshot(snapshot => {
       //console.log(change.type, change.doc.id, change.doc.data());
       if(change.type === 'added'){
         // add the document data to the web page
+        renderRecipe(change.doc.data(), change.doc.id);
       }
       if(change.type === 'removed'){
         // remove the document data from the web page
