f03e77cbf902e86704c130e41747400daa9057f4
wissmedia <44659148+wissmedia@users.noreply.github.com>
2020-01-15 20:24:46 +0800

    .

--- sw.js
+++ sw.js
@@ -52,21 +52,21 @@ self.addEventListener('activate', evt => {
 
 // fetch event
 self.addEventListener('fetch', evt => {
-  //console.log('fetch event', evt);
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request).then(fetchRes => {
-        return caches.open(dynamicCacheName).then(cache => {
-          cache.put(evt.request.url, fetchRes.clone());
-          // check cached items size
-          limitCacheSize(dynamicCacheName, 15);
-          return fetchRes;
-        })
-      });
-    }).catch(() => {
-      if(evt.request.url.indexOf('.html') > -1){
-        return caches.match('/pages/fallback.html');
-      }
-    })
-  );
+  // //console.log('fetch event', evt);
+  // evt.respondWith(
+  //   caches.match(evt.request).then(cacheRes => {
+  //     return cacheRes || fetch(evt.request).then(fetchRes => {
+  //       return caches.open(dynamicCacheName).then(cache => {
+  //         cache.put(evt.request.url, fetchRes.clone());
+  //         // check cached items size
+  //         limitCacheSize(dynamicCacheName, 15);
+  //         return fetchRes;
+  //       })
+  //     });
+  //   }).catch(() => {
+  //     if(evt.request.url.indexOf('.html') > -1){
+  //       return caches.match('/pages/fallback.html');
+  //     }
+  //   })
+  // );
 });
