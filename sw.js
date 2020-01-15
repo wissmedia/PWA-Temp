226ccdfa7c96bcdb0a04a2ce8d523e6d77188809
wissmedia <44659148+wissmedia@users.noreply.github.com>
2020-01-15 19:47:03 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,29 @@
-const staticCacheName = "site-static-v1";
-const dynamicCacheName = 'site-dynamic-v1';
-
+const staticCacheName = 'site-static-v1.1';
+const dynamicCacheName = 'site-dynamic-v1.1';
 const assets = [
-  "/",
-  "/index.html",
-  "/js/app.js",
-  "/js/ui.js",
-  "/js/materialize.min.js",
-  "/css/styles.css",
-  "/css/materialize.min.css",
-  "/img/dish.png",
-  "https://fonts.googleapis.com/icon?family=Material+Icons",
-  "https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2"
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/js/materialize.min.js',
+  '/css/styles.css',
+  '/css/materialize.min.css',
+  '/img/dish.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+  '/pages/fallback.html'
 ];
+
+// cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if(keys.length > size){
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
 
 // install event
 self.addEventListener('install', evt => {
@@ -47,9 +58,15 @@ self.addEventListener('fetch', evt => {
       return cacheRes || fetch(evt.request).then(fetchRes => {
         return caches.open(dynamicCacheName).then(cache => {
           cache.put(evt.request.url, fetchRes.clone());
+          // check cached items size
+          limitCacheSize(dynamicCacheName, 15);
           return fetchRes;
         })
       });
+    }).catch(() => {
+      if(evt.request.url.indexOf('.html') > -1){
+        return caches.match('/pages/fallback.html');
+      }
     })
   );
 });
